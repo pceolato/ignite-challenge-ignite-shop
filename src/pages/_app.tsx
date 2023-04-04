4c847bd9106aacbc7b233d47128c09ff6c2bb4b8
Pedro <pedroceolato2002@gmail.com>
2023-04-04 18:48:29 -0300

    feat: add DrawerContextProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,29 @@ import { Handbag } from "@phosphor-icons/react";
 import logoImg from '../assets/logo.svg'
 import { ButtonBag, Container, Header } from "../styles/pages/app";
 import Image from "next/image";
+import { DrawerContextProvider, useDrawer } from "../context/DrawerContext";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Container>
-      <Header>
-        <Image src={logoImg.src} width={130} height={52} alt="" />
-        <ButtonBag>
-          <Handbag size={24} />
-        </ButtonBag>
-      </Header>
+  const { handleSetDrawerIsOpen } = useDrawer()
 
-      <Component {...pageProps} />
-    </Container>
+  function handle() {
+    handleSetDrawerIsOpen()
+  } 
+
+  return (
+    <DrawerContextProvider>
+      <Container>
+        <Header>
+          <Image src={logoImg.src} width={130} height={52} alt="" />
+          <ButtonBag onClick={handle}>
+            <Handbag size={24} />
+          </ButtonBag>
+        </Header>
+
+        <Component {...pageProps} />
+      </Container>
+    </DrawerContextProvider>
   )
 }
