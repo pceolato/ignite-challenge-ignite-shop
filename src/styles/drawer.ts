a6e973b74f0bef0b4e4f5a30cc5c0aed535cd371
Pedro <pedroceolato2002@gmail.com>
2023-04-04 18:47:54 -0300

    style: add DrawerContent

---
+++ src/styles/drawer.ts
@@ -0,0 +1,12 @@
+import { styled } from "@stitches/react";
+
+export const DrawerContent = styled('div', {
+    height: '100vh',
+    position: 'fixed',
+    zIndex: '1000',
+    width: '400px',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    background: '$gray800'
+})
