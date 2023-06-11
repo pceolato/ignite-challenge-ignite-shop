ac1f016fc602d70833f55dfe78689475d3e03633
Pedro <pedroceolato2002@gmail.com>
2023-06-11 19:36:15 -0300

    refactor: clean with components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,18 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
-import { Handbag } from "@phosphor-icons/react";
-
-import logoImg from '../assets/logo.svg'
-import { ButtonBag, Container, Header } from "../styles/pages/app";
-import Image from "next/image";
+import { Container } from "../styles/pages/app";
 import { DrawerContextProvider, useDrawer } from "../context/DrawerContext";
+import { HeaderNav } from "../components/Header";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { handleSetDrawerIsOpen } = useDrawer()
-
-  function handle() {
-    handleSetDrawerIsOpen()
-  } 
 
   return (
     <DrawerContextProvider>
       <Container>
-        <Header>
-          <Image src={logoImg.src} width={130} height={52} alt="" />
-          <ButtonBag onClick={handle}>
-            <Handbag size={24} />
-          </ButtonBag>
-        </Header>
-
+        <HeaderNav />
         <Component {...pageProps} />
       </Container>
     </DrawerContextProvider>
