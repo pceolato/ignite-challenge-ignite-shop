2e5d38d4ead2397724ecd06b0d059467f6af84db
Pedro <pedroceolato2002@gmail.com>
2023-07-24 19:07:25 -0300

    feat: added count bag in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import { ButtonBag, Header } from "../styles/pages/app";
+import { ButtonBag, CountBag, Header } from "../styles/pages/app";
 import { Handbag } from "@phosphor-icons/react";
 
 import logoImg from '../assets/logo.svg'
 import { useDrawer } from "../context/DrawerContext";
+import { useCart } from "../hooks/useCart";
 
 export function HeaderNav() {
     const { handleSetDrawer } = useDrawer()
+    const { cartItems } = useCart()
 
     function handleOpenCart() {
         handleSetDrawer(true)
@@ -16,7 +18,14 @@ export function HeaderNav() {
         <Header>
             <Image src={logoImg.src} width={130} height={52} alt="" />
             <ButtonBag onClick={handleOpenCart}>
-            <Handbag size={24} />
+                {cartItems.length > 0 && (
+                    <CountBag>
+                        <span>
+                            {cartItems.length}
+                        </span>
+                    </CountBag>
+                )}
+                <Handbag size={24} />
             </ButtonBag>
         </Header>
     )
