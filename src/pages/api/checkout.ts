240f4240cd9b168fc220d8481d856e90c37dc462
Pedro <pedroceolato2002@gmail.com>
2023-08-13 21:05:43 -0300

    feat: change checkout to get products

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,15 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
+import { IProduct } from "../../context/CartContext";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { priceId } = req.body;
+    const { products } = req.body as { products: IProduct[] };
 
     if(req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' })
     }
  
-    if(!priceId)  {
-        return res.status(400).json({ error: 'Price not found.' })
+    if(!products)  {
+        return res.status(400).json({ error: 'Products not found.' })
     }
 
     const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
@@ -19,12 +20,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         success_url: successUrl,
         cancel_url: cancelUrl,
         mode: 'payment',
-        line_items: [
-            {
-                price: priceId,
+        line_items: products.map(product => {
+            return {
+                price: product.defaultPrice,
                 quantity: 1
-            },
-        ]
+            }
+        })
     })
 
     return res.status(201).json({
